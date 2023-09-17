6b1dea604cfeba3bc881f7f99a8ed45563265cd5
Firmansyah Jaya Kusuma <88803824+Firmansjk@users.noreply.github.com>
2023-09-17 13:32:36 +0800

    HighScore Scene proto

--- Assets/Scripts/SceneMoving.cs
+++ Assets/Scripts/SceneMoving.cs
@@ -9,4 +9,14 @@ public class SceneMoving : MonoBehaviour
     {
         SceneManager.LoadScene("PreparationScene", LoadSceneMode.Single);
     }
+
+    public void GoToMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Single);
+    }
+
+    public void LoadHighscoreScene()
+    {
+        SceneManager.LoadScene("AfterHighscoreScene", LoadSceneMode.Single);
+    }
 }
