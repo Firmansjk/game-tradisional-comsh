41163f215585a83f87667503911d4cb6331e8f26
Firmansyah Jaya Kusuma <88803824+Firmansjk@users.noreply.github.com>
2023-09-21 03:57:52 +0800

    latto latto Slider system Update

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -13,12 +13,6 @@ public class GameTimer : MonoBehaviour
     public bool timeIsRunning = false;
     public Text timerText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scripts/Latto Latto Scripts/PlayerOneSliderControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerOneSliderControl : MonoBehaviour
+{
+    public Slider sliderObject;
+    public Text scoreText;
+    public int score = 0;
+
+    void Start()
+    {
+        sliderObject.minValue = 0;
+        sliderObject.maxValue = 100;
+        sliderObject.wholeNumbers = true;
+        sliderObject.value = 50;
+    }
+
+    public void OnValueChange(float value)
+    {
+        if (sliderObject.value == 100)
+        {
+            score += 1;
+            scoreText.text = score.ToString();
+        }
+    }
+}
